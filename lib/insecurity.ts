4e1cc9b85d27accf9cf33a25e51f73257d911839
Denys <denys@gmail.com>
2025-11-27 22:11:31 +0200

    Усунуто вразливості Password Management: Insecure Submission та Cookie Security: Cookie not Sent Over SSL

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -195,7 +195,11 @@ export const updateAuthenticatedUsers = () => (req: Request, res: Response, next
       if (err === null) {
         if (authenticatedUsers.get(token) === undefined) {
           authenticatedUsers.put(token, decoded)
-          res.cookie('token', token)
+          res.cookie('token', token, {
+              httpOnly: true,
+              secure: true,
+              sameSite: 'Strict'
+              })
         }
       }
     })
